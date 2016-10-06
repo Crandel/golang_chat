a6c38457feb41af3b8b141a6b1c40842f1a724fc
Crandel <cradlemann@gmail.com>
2016-10-06 18:26:36 +0300

    Add gorm

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	router "github.com/julienschmidt/httprouter"
 )
 
@@ -31,22 +32,28 @@ var C Config
 func main() {
 	Debug.Print("Start Server on 8080 port logger")
 	r := router.New()
+	configFile, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		Error.Println("opening config file", err.Error())
+	}
+	err = json.Unmarshal(configFile, &C)
+	if err != nil {
+		Error.Println("parsing json", err.Error())
+	}
+	Debug.Println(C)
+	db, err := gorm.Open("postgres", C.Database)
+	defer db.Close()
+	// Automigration of tables
+	db.AutoMigrate(&User{}, &Message{})
+
 	r.GET("/main", func(w http.ResponseWriter, r *http.Request, p router.Params) {
-		configFile, err := ioutil.ReadFile("config.json")
-		if err != nil {
-			Error.Println("opening config file", err.Error())
-		}
-		err = json.Unmarshal(configFile, &C)
-		if err != nil {
-			Error.Println("parsing json", err.Error())
-		}
-		Debug.Println(C)
-		firstUser := User{1, "example@mail.com", "first login", "pass1"}
-		secondUser := User{2, "2example@mail.com", "second login", "pass2"}
-		firstMessage := Message{firstUser, "first Message from first user", time.Now()}
-		secondMessage := Message{secondUser, "first Message from second user", time.Now()}
-		messages := []Message{firstMessage, secondMessage}
-		Debug.Println(messages)
+		// firstUser := User{1, "example@mail.com", "first login", "pass1"}
+		// secondUser := User{2, "2example@mail.com", "second login", "pass2"}
+		// firstMessage := Message{firstUser, "first Message from first user", time.Now()}
+		// secondMessage := Message{secondUser, "first Message from second user", time.Now()}
+		// messages := []Message{firstMessage, secondMessage}
+		// Debug.Println(messages)
+		messages := []Message{}
 		Socket()
 		base := template.Must(template.ParseFiles("./templates/base.gotmpl", "./templates/main.gotmpl"))
 		base.Execute(w, messages)
--- models.go
+++ models.go
@@ -1,18 +1,23 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 // User model from database
 type User struct {
-	ID       int
-	Email    string
-	Login    string
-	Password string
+	gorm.Model
+	Email    string `gorm:"size:50"`
+	Login    string `gorm:"size:50"`
+	Password string `gorm:"size:255"`
+	Messages []Message
 }
 
 // Message from database
 type Message struct {
-	User    User
+	gorm.Model
 	Message string
 	Time    time.Time
 }
