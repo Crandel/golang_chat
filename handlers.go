c8f468ff0e4016898592df1d11bb0983fab09328
Crandel <cradlemann@gmail.com>
2016-10-10 16:07:53 +0300

    Improve config to parse templates, fix time in templates

--- handlers.go
+++ handlers.go
@@ -7,18 +7,25 @@ import (
 	router "github.com/julienschmidt/httprouter"
 )
 
-var messages []Message
+var (
+	messages []Message
+)
 
 // PageMainHandler handler for main page
 func PageMainHandler(w http.ResponseWriter, r *http.Request, _ router.Params) {
-	Db.Find(&messages)
+	Db.Preload("User").Find(&messages)
 	Socket()
 	main := template.Must(template.ParseFiles("./templates/base.gotmpl", "./templates/main.gotmpl"))
 	main.Execute(w, messages)
 }
 
-// LoginHandler ...
-func LoginHandler(w http.ResponseWriter, r *http.Request, _ router.Params) {
+// GetLoginHandler - render template for login page
+func GetLoginHandler(w http.ResponseWriter, r *http.Request, _ router.Params) {
 	login := template.Must(template.ParseFiles("./templates/base.gotmpl", "./templates/login.gotmpl"))
 	login.Execute(w, nil)
+}
+
+// PostLoginHandler - parce form, validate and save user
+func PostLoginHandler(w http.ResponseWriter, r *http.Request, p router.Params) {
+	// validate form here
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -26,25 +27,61 @@ var (
 
 // Config struct
 type config struct {
-	Database string `json:"Database"`
-	Version  string `json:"Version"`
-	Host     string `json:"Host"`
-	Port     string `json:"Port"`
+	Database database  `json:"Database"`
+	Version  string    `json:"Version"`
+	Host     string    `json:"Host"`
+	Port     string    `json:"Port"`
+	Template templates `json:"Template"`
+}
+
+// Struct for templates
+type templates struct {
+	Root        string
+	Ext         string
+	Folder      string
+	TemplateMap map[string]string
+}
+
+type database struct {
+	Type string
+	Path string
 }
 
 func init() {
+	// We need to parse config json file into Config struct
 	configFile, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		Error.Println("opening config file", err.Error())
+		Error.Println("Opening config file", err.Error())
 	}
 	err = json.Unmarshal(configFile, &Config)
 	if err != nil {
-		Error.Println("parsing json", err.Error())
+		Error.Println("Parsing json", err.Error())
 	}
+	// Fill template struct from our templates dir
+	templ := "%s/%s.%s"
+	templateDir := fmt.Sprintf("./%s", Config.Template.Folder)
+	// list of files from templates dir
+	files, err := ioutil.ReadDir(templateDir)
+	if err != nil {
+		Error.Println("Read template Directory", err.Error())
+	}
+	// Set template Root file, all templates will Parse with it
+	Config.Template.Root = fmt.Sprintf(templ, Config.Template.Folder, Config.Template.Root, Config.Template.Ext)
+	// Create map with template names
+	templates := make(map[string]string)
+	for _, f := range files {
+		ext := fmt.Sprintf(".%s", Config.Template.Ext)
+		fullName := f.Name()
+		index := strings.Index(fullName, ext)
+		// name without extension
+		name := fullName[:index]
+		templates[name] = fullName
+	}
+	Config.Template.TemplateMap = templates
 }
 
 func main() {
-	Db, _ = gorm.Open("postgres", Config.Database)
+	Db, _ = gorm.Open(Config.Database.Type, Config.Database.Path)
 	defer Db.Close()
 	Db.LogMode(true)
 	// Automigration of tables
@@ -55,15 +92,17 @@ func main() {
 	// Create new httprouter for ListenAndServe http loop
 	r := router.New()
 	Debug.Printf("Start Server version %s on %s:%s", Config.Version, Config.Host, Config.Port)
-	user1 := User{
-		Email:    "cradlemann@gmail.com",
-		Login:    "crandel",
-		Password: "pass",
-		Messages: []Message{{Message: "First message"}, {Message: "second mess"}},
-	}
-	Debug.Println(user1)
-	Db.Set("gorm:save_associations", true).Create(&user1)
+	// user1 := User{
+	// 	Email:    "cradlemann@gmail.com",
+	// 	Login:    "crandel",
+	// 	Password: "pass",
+	// 	Messages: []Message{{Message: "First message"}, {Message: "second mess"}},
+	// }
+	// Debug.Println(user1)
+	// Db.Set("gorm:save_associations", true).Create(&user1)
 	r.GET("/", PageMainHandler)
+	r.GET("/login", GetLoginHandler)
+	r.POST("/login", PostLoginHandler)
 	r.ServeFiles("/static/*filepath", http.Dir("./public"))
 	server := fmt.Sprintf("%s:%s", Config.Host, Config.Port)
 	Error.Println(http.ListenAndServe(server, r))
